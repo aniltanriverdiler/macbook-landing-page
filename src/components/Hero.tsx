72a4b4584246b9ff5ed57e7bce558ac0b0eef667
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-02-12 14:29:55 +0300

    feat: implement Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 2;
+  }, []);
+
+  return (
+    <section id="hero">
+      {/* Hero Title */}
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="/public/title.png" alt="MacBook Title" />
+      </div>
+
+      {/* Hero Video */}
+      <video
+        ref={videoRef}
+        src="/public/videos/hero.mp4"
+        autoPlay
+        muted
+        playsInline
+      />
+
+      {/* Buy Button */}
+      <button>Buy</button>
+
+      {/* Hero Description */}
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  );
+};
+
+export default Hero;
